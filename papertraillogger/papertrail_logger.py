262e22138d3320d65b35bf3230e90c1a0197248a
mirco.gnuva@studenti.unimi.it <miccomicco1997>
2019-03-15 20:16:56 +0100

    Now the PapertrailLogger class works as intended :)

--- papertraillogger/papertrail_logger.py
+++ papertraillogger/papertrail_logger.py
@@ -4,20 +4,38 @@ from logging.handlers import SysLogHandler
 
 
 class ContextFilter(logging.Filter):
-    hostname = socket.gethostname()
+	hostname = socket.gethostname()
 
-    def filter(self, record):
-        record.hostname = ContextFilter.hostname
-        return True
+	def filter(self, record):
+		record.hostname = ContextFilter.hostname
+		return True
 
 
-class Logger:
-    def __init__(self, url, code, app_name, logging_level):
-        syslog = SysLogHandler(address=(url, code))
-        syslog.addFilter(ContextFilter())
-        format = "%(asctime)s %(levelname)s from %(hostname)s " + app_name + " (%(process)d): %(message)s"
-        formatter = logging.Formatter(format, datefmt='%b %d %H:%M:%S')
-        syslog.setFormatter(formatter)
-        logger = logging.getLogger()
-        logger.addHandler(syslog)
-        logger.setLevel(logging_level)
+class PapertrailLogger:
+	def __init__(self, destination, app_name, logging_level):
+		self.logger = logging.getLogger()
+		self.destination_url = destination.split(":")[0]
+		self.destination_code = int(destination.split(":")[1])
+		self.syslog = SysLogHandler(address=(self.destination_url, self.destination_code))
+		self.syslog.addFilter(ContextFilter())
+		self.format = "%(asctime)s %(levelname)s %(hostname)s " + app_name + " (%(process)d): %(message)s"
+		self.formatter = logging.Formatter(self.format, datefmt='%b %d %H:%M:%S')
+		self.syslog.setFormatter(self.formatter)
+		self.logger = logging.getLogger()
+		self.logger.addHandler(self.syslog)
+		self.logger.setLevel(logging_level)
+
+	def debug(self, msg):
+		self.logger.debug(msg)
+
+	def info(self, msg):
+		self.logger.info(msg)
+
+	def warning(self, msg):
+		self.logger.warning(msg)
+
+	def error(self, msg):
+		self.logger.error(msg)
+
+	def critical(self, msg):
+		self.logger.critical(msg)
